b0500cbe007e485b070570665c61d1dd9201cfd5
dlamaral <dlamaral12@gmail.com>
2021-05-25 22:36:40 +0100

    Added second context menu for Greenhouse search

--- eventPage.js
+++ eventPage.js
@@ -3,11 +3,25 @@ var contextMenuItem = {
     "title": "Search for social media",
     "contexts": ["selection"]
 };
+
 chrome.contextMenus.create(contextMenuItem);
+var contextMenuItem2 = {
+    "id": "searchGh",
+    "title": "Search in Greenhouse",
+    "contexts": ["selection"]
+};
+chrome.contextMenus.create(contextMenuItem2);
 
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    chrome.tabs.create({  
-        url: "https://www.google.com/search?q=(site:linkedin.com/in+OR+site:facebook.com+OR+site:instagram.com+OR+site:twitter.com+OR+site:tiktok.com)+" + encodeURIComponent(info.selectionText)
-    });
+    if (info.menuItemId === "searchSocial"){
+        chrome.tabs.create({  
+            url: "https://www.google.com/search?q=(site:linkedin.com/in+OR+site:facebook.com+OR+site:instagram.com+OR+site:twitter.com+OR+site:tiktok.com)+" + encodeURIComponent(info.selectionText)
+        });
+    }
+    if (info.menuItemId === "searchGh"){
+        chrome.tabs.create({
+            url: "https://app4.greenhouse.io/people?job_status=all&search_terms=" + encodeURIComponent(info.selectionText) + "&sort_by=last_activity&sort_order=desc&type=all"
+        })
+    }
 })
